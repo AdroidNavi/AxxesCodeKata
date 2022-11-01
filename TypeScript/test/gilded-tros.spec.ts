63d01f06bc10cd137d43847e054ac362cc689e1c
Arnold de Roij <ahrl.de.roij@gmail.com>
2022-11-01 20:13:27 +0100

    imclude quality and sellIn in the test

--- TypeScript/test/gilded-tros.spec.ts
+++ TypeScript/test/gilded-tros.spec.ts
@@ -2,8 +2,10 @@ import { Item } from "../src/item";
 import { GildedTros } from "../src/gilded-tros";
 
 it("GildedTrosTest", () => {
-  const items: Item[] = [new Item("foo", 0, 0)];
+  const items: Item[] = [new Item("Good Wine", 10, 10)];
   const app: GildedTros = new GildedTros(items);
   app.updateQuality();
-  expect(app.items[0].name).toEqual("foo");
+  expect(app.items[0].name).toEqual("Good Wine");
+  expect(app.items[0].quality).toEqual(11);
+  expect(app.items[0].sellIn).toEqual(9);
 });
