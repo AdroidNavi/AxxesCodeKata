0964f389034f41b957598ced95f0b91862a56a1b
Arnold de Roij <ahrl.de.roij@gmail.com>
2022-11-01 20:59:15 +0100

    add tests for all existing scenarios

--- TypeScript/test/gilded-tros.spec.ts
+++ TypeScript/test/gilded-tros.spec.ts
@@ -3,19 +3,74 @@ import { GildedTros } from "../src/gilded-tros";
 
 describe("GildedTros", () => {
   it.each`
-    name                                | expectedSellIn | expectedQuality
-    ${"Good Wine"}                      | ${9}           | ${11}
-    ${"B-DAWG Keychain"}                | ${10}          | ${80}
-    ${"Backstage passes for Re:Factor"} | ${9}           | ${12}
-    ${"Backstage passes for HAXX"}      | ${9}           | ${12}
-  `("sellIn 10 days | $name", ({ name, expectedSellIn, expectedQuality }) => {
-    const items: Item[] = [new Item(name, 10, 10)];
-    const app: GildedTros = new GildedTros(items);
+    name                                 | sellIn | expectedSellIn | expectedQuality
+    ${"Test Item"}                       | ${10}  | ${9}           | ${9}
+    ${"Test Item"}                       | ${0}   | ${-1}          | ${8}
+    ${"Good Wine"}                       | ${10}  | ${9}           | ${11}
+    ${"B-DAWG Keychain"}                 | ${10}  | ${10}          | ${80}
+    ${"Backstage passes for Re:Factor"}  | ${11}  | ${10}          | ${11}
+    ${"Backstage passes for Re:Factor"}  | ${10}  | ${9}           | ${12}
+    ${"Backstage passes for Re:Factor"}  | ${6}   | ${5}           | ${12}
+    ${"Backstage passes for Re:Factor"}  | ${5}   | ${4}           | ${13}
+    ${"Backstage passes for Re:Factor"}  | ${0}   | ${-1}          | ${0}
+    ${"Backstage passes for HAXX"}       | ${11}  | ${10}          | ${11}
+    ${"Backstage passes for HAXX"}       | ${10}  | ${9}           | ${12}
+    ${"Backstage passes for HAXX"}       | ${6}   | ${5}           | ${12}
+    ${"Backstage passes for HAXX"}       | ${5}   | ${4}           | ${13}
+    ${"Backstage passes for HAXX"}       | ${0}   | ${-1}          | ${0}
+    ${"Backstage passes for Test Event"} | ${11}  | ${10}          | ${11}
+    ${"Backstage passes for Test Event"} | ${10}  | ${9}           | ${12}
+    ${"Backstage passes for Test Event"} | ${6}   | ${5}           | ${12}
+    ${"Backstage passes for Test Event"} | ${5}   | ${4}           | ${13}
+    ${"Backstage passes for Test Event"} | ${0}   | ${-1}          | ${0}
+  `(
+    "sellIn $sellIn day(s) | $name",
+    ({ name, sellIn, expectedSellIn, expectedQuality }) => {
+      const items: Item[] = [new Item(name, sellIn, 10)];
+      const app: GildedTros = new GildedTros(items);
 
-    app.updateQuality();
+      app.updateQuality();
 
-    expect(app.items).toEqual([
-      new Item(name, expectedSellIn, expectedQuality),
-    ]);
-  });
+      expect(app.items).toEqual([
+        new Item(name, expectedSellIn, expectedQuality),
+      ]);
+    }
+  );
+
+  it.each`
+    name           | expectedSellIn | expectedQuality
+    ${"Test Item"} | ${9}           | ${0}
+  `(
+    "quality never drops below 0 | $name",
+    ({ name, expectedSellIn, expectedQuality }) => {
+      const items: Item[] = [new Item(name, 10, 0)];
+      const app: GildedTros = new GildedTros(items);
+
+      app.updateQuality();
+
+      expect(app.items).toEqual([
+        new Item(name, expectedSellIn, expectedQuality),
+      ]);
+    }
+  );
+
+  it.each`
+    name                                 | expectedSellIn | expectedQuality
+    ${"Good Wine"}                       | ${9}           | ${50}
+    ${"Backstage passes for Re:Factor"}  | ${9}           | ${50}
+    ${"Backstage passes for HAXX"}       | ${9}           | ${50}
+    ${"Backstage passes for Test Event"} | ${9}           | ${50}
+  `(
+    "quality never goes over 50 | $name",
+    ({ name, expectedSellIn, expectedQuality }) => {
+      const items: Item[] = [new Item(name, 10, 50)];
+      const app: GildedTros = new GildedTros(items);
+
+      app.updateQuality();
+
+      expect(app.items).toEqual([
+        new Item(name, expectedSellIn, expectedQuality),
+      ]);
+    }
+  );
 });
