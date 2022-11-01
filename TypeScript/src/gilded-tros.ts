dded3d94ebfc721b1125a9274e996a01663fb78b
Arnold de Roij <ahrl.de.roij@gmail.com>
2022-11-01 21:01:08 +0100

    fixed tests

--- TypeScript/src/gilded-tros.ts
+++ TypeScript/src/gilded-tros.ts
@@ -7,8 +7,7 @@ export class GildedTros {
     for (let i = 0; i < this.items.length; i++) {
       if (
         this.items[i].name != "Good Wine" &&
-        this.items[i].name != "Backstage passes for Re:Factor" &&
-        this.items[i].name != "Backstage passes for HAXX"
+        !this.items[i].name.startsWith("Backstage passes")
       ) {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != "B-DAWG Keychain") {
@@ -21,10 +20,7 @@ export class GildedTros {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
 
-          if (
-            this.items[i].name == "Backstage passes for Re:Factor" ||
-            this.items[i].name == "Backstage passes for HAXX"
-          ) {
+          if (this.items[i].name.startsWith("Backstage passes")) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
@@ -46,10 +42,7 @@ export class GildedTros {
 
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != "Good Wine") {
-          if (
-            this.items[i].name != "Backstage passes for Re:Factor" ||
-            this.items[i].name != "Backstage passes for HAXX"
-          ) {
+          if (!this.items[i].name.startsWith("Backstage passes")) {
             if (this.items[i].quality > 0) {
               if (this.items[i].name != "B-DAWG Keychain") {
                 this.items[i].quality = this.items[i].quality - 1;
