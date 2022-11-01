b3193636d9095e549a6872cad2783028020023f9
Arnold de Roij <ahrl.de.roij@gmail.com>
2022-11-01 20:43:09 +0100

    reorder parameters to sync with Item api

--- TypeScript/test/gilded-tros.spec.ts
+++ TypeScript/test/gilded-tros.spec.ts
@@ -3,12 +3,12 @@ import { GildedTros } from "../src/gilded-tros";
 
 describe("GildedTros", () => {
   it.each`
-    name                                | expectedQuality | expectedSellIn
-    ${"Good Wine"}                      | ${11}           | ${9}
-    ${"B-DAWG Keychain"}                | ${80}           | ${10}
-    ${"Backstage passes for Re:Factor"} | ${12}           | ${9}
-    ${"Backstage passes for HAXX"}      | ${12}           | ${9}
-  `("sellIn 10 days | $name", ({ name, expectedQuality, expectedSellIn }) => {
+    name                                | expectedSellIn | expectedQuality
+    ${"Good Wine"}                      | ${9}           | ${11}
+    ${"B-DAWG Keychain"}                | ${10}          | ${80}
+    ${"Backstage passes for Re:Factor"} | ${9}           | ${12}
+    ${"Backstage passes for HAXX"}      | ${9}           | ${12}
+  `("sellIn 10 days | $name", ({ name, expectedSellIn, expectedQuality }) => {
     const items: Item[] = [new Item(name, 10, 10)];
     const app: GildedTros = new GildedTros(items);
 
