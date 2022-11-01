d4c5c1ec1632084453992ac9838b8dc28d09b243
Arnold de Roij <ahrl.de.roij@gmail.com>
2022-11-01 20:35:41 +0100

    add tests for each existing item

--- TypeScript/test/gilded-tros.spec.ts
+++ TypeScript/test/gilded-tros.spec.ts
@@ -1,11 +1,22 @@
 import { Item } from "../src/item";
 import { GildedTros } from "../src/gilded-tros";
 
-it("GildedTrosTest", () => {
-  const items: Item[] = [new Item("Good Wine", 10, 10)];
-  const app: GildedTros = new GildedTros(items);
-  app.updateQuality();
-  expect(app.items[0].name).toEqual("Good Wine");
-  expect(app.items[0].quality).toEqual(11);
-  expect(app.items[0].sellIn).toEqual(9);
-});
+it.each`
+  name                                | expectedQuality | expectedSellIn
+  ${"Good Wine"}                      | ${11}           | ${9}
+  ${"B-DAWG Keychain"}                | ${10}           | ${10}
+  ${"Backstage passes for Re:Factor"} | ${12}           | ${9}
+  ${"Backstage passes for HAXX"}      | ${11}           | ${9}
+`(
+  "GildedTros | one day | $name",
+  ({ name, expectedQuality, expectedSellIn }) => {
+    const items: Item[] = [new Item(name, 10, 10)];
+    const app: GildedTros = new GildedTros(items);
+
+    app.updateQuality();
+
+    expect(app.items).toEqual([
+      new Item(name, expectedSellIn, expectedQuality),
+    ]);
+  }
+);
