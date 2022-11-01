077883e954df6c3b7b4f7a24fb84a28457fcc193
Arnold de Roij <ahrl.de.roij@gmail.com>
2022-11-01 22:26:22 +0100

    cleanup workflow

--- TypeScript/src/gilded-tros.ts
+++ TypeScript/src/gilded-tros.ts
@@ -1,61 +1,77 @@
 import { Item } from "./item";
 
+type ItemCategory = {
+  predicate: (item: Item) => boolean;
+  updater: (item: Item) => Item;
+};
+
+const BdawgKeychain = "B-DAWG Keychain";
+const GoodWine = "Good Wine";
+const BackstagePasses = "Backstage passes";
 const CodeSmellItemNames = [
   "Duplicate Code",
   "Long Methods",
   "Ugly Variable Names",
 ];
 
+const categories: ItemCategory[] = [
+  {
+    predicate: (item) => item.name === BdawgKeychain,
+    updater: (item) => ({ ...item, quality: 80 }),
+  },
+  {
+    predicate: (item) => item.name === GoodWine,
+    updater: (item) => itemUpdater(item, 1),
+  },
+  {
+    predicate: (item) => item.name.startsWith(BackstagePasses),
+    updater: (item) => backstagePassesUpdater(item),
+  },
+  {
+    predicate: (item) => CodeSmellItemNames.includes(item.name),
+    updater: (item) => itemUpdater(item, -2),
+  },
+];
+
+const backstagePassesUpdater = (item: Item): Item => {
+  const { sellIn } = item;
+  if (sellIn <= 0) {
+    return { ...item, sellIn: sellIn - 1, quality: 0 };
+  }
+
+  if (sellIn <= 5) {
+    return itemUpdater(item, 3);
+  }
+
+  if (sellIn <= 10) {
+    return itemUpdater(item, 2);
+  }
+
+  return itemUpdater(item, 1);
+};
+
+const itemUpdater = (item: Item, qualityDelta: number): Item => {
+  const qualityPlusDelta =
+    item.quality + (item.sellIn <= 0 ? qualityDelta * 2 : qualityDelta);
+
+  const newQuality = Math.max(0, Math.min(50, qualityPlusDelta));
+
+  return {
+    name: item.name,
+    sellIn: item.sellIn - 1,
+    quality: newQuality,
+  };
+};
+
 export class GildedTros {
   constructor(public items: Array<Item>) {}
 
-  private updateItem(item: Item, qualityChange: number): Item {
-    const realQualityChange =
-      item.sellIn <= 0 ? qualityChange * 2 : qualityChange;
-    const newQuality = Math.max(
-      0,
-      Math.min(50, item.quality + realQualityChange)
-    );
-
-    return {
-      ...item,
-      sellIn: item.sellIn - 1,
-      quality: newQuality,
-    };
-  }
-
   public updateItems(): void {
     this.items = this.items.map((item) => {
-      const { name, sellIn } = item;
-      if (name === "B-DAWG Keychain") {
-        return { ...item, quality: 80 };
-      }
-
-      if (name === "Good Wine") {
-        return this.updateItem(item, 1);
-      }
-
-      if (name.startsWith("Backstage passes")) {
-        if (sellIn <= 0) {
-          return { ...item, sellIn: sellIn - 1, quality: 0 };
-        }
-
-        if (sellIn <= 5) {
-          return this.updateItem(item, 3);
-        }
-
-        if (sellIn <= 10) {
-          return this.updateItem(item, 2);
-        }
-
-        return this.updateItem(item, 1);
-      }
-
-      if (CodeSmellItemNames.includes(name)) {
-        return this.updateItem(item, -2);
-      }
-
-      return this.updateItem(item, -1);
+      const itemCategory = categories.find((category) =>
+        category.predicate(item)
+      );
+      return itemCategory ? itemCategory.updater(item) : itemUpdater(item, -1);
     });
   }
 }
